b6812f646931df6eee3b646030c680feb220063b
Aryan Bagane <aryanbagane30@gmail.com>
2024-01-31 11:19:27 +0530

    added employee field

--- api/models.py
+++ api/models.py
@@ -13,5 +13,15 @@ class Company(models.Model):
     added_date = models.DateTimeField(auto_now = True)
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 # Creating Employee model
-    
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    number = models.CharField(max_length=10)
+    about = models.TextField(max_length=200)
+    position = models.CharField(max_length=50, choices = (('Manager','manager'), ('Tech Lead','tech lead'), ('Developer','developer')))
+    company = models.ForeignKey(Company, on_delete = models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Company
-from api.serializers import CompanySerializer
+from api.models import Company, Employee
+from api.serializers import CompanySerializer, EmployeeSerializer
 
 # Create your views here.
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
