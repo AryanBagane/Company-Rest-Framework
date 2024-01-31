968017bb470d3745ed1cd291520d030a66a8736d
Aryan Bagane <aryanbagane30@gmail.com>
2024-01-31 12:36:12 +0530

    Disabling browsable API

--- companyapi/settings.py
+++ companyapi/settings.py
@@ -129,5 +129,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer'
+    ) 
+    # Disabling browsable API
 }
